bf9c4318a0217503f72c49ce8e1dc88d5889e03d
Jan Klimo <jan.klimo@gmail.com>
2015-08-12 11:38:03 +0700

    lean timer component receiving props

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import * as TimeUtils from './utils/time.js';
 import Timer from './components/timer.jsx'
 
 const App = React.createClass({
+  getInitialState(){
+    return {
+      hLeft: 0,
+      mLeft: 0,
+      sLeft: 0,
+      isBeerOClock: false,
+      msLeft: 0
+    };
+  },
+  componentDidMount(){
+    this.tick();
+    this.timer = setInterval(this.tick, 250);
+  },
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  },
+  tick() {
+    let currentTime = new Date();
+    let endTime = TimeUtils.nearestLimit(currentTime);
+
+    let { hLeft, mLeft, sLeft, msLeft,
+      isBeerOClock } = TimeUtils.fetchCountdown(currentTime, endTime);
+
+    this.setState({ hLeft, mLeft, sLeft, msLeft, isBeerOClock });
+  },
   render() {
     return <div>
       <h2>Is it beer o'clock yet?</h2>
-      <Timer />
+      <Timer {...this.state} />
     </div>
   }
 });
--- src/js/components/timer.jsx
+++ src/js/components/timer.jsx
@@ -1,34 +1,8 @@
 import React from 'react';
-import * as TimeUtils from '../utils/time.js';
 
 export default React.createClass({
-  getInitialState(){
-    return {
-      hLeft: 0,
-      mLeft: 0,
-      sLeft: 0,
-      isBeerOClock: false,
-      msLeft: 0
-    };
-  },
-  componentDidMount(){
-    this.tick();
-    this.timer = setInterval(this.tick, 250);
-  },
-  componentWillUnmount(){
-    clearInterval(this.timer);
-  },
-  tick() {
-    let currentTime = new Date();
-    let endTime = TimeUtils.nearestLimit(currentTime);
-
-    let { hLeft, mLeft, sLeft, msLeft,
-      isBeerOClock } = TimeUtils.fetchCountdown(currentTime, endTime);
-
-    this.setState({ hLeft, mLeft, sLeft, msLeft, isBeerOClock });
-  },
   render() {
-    let { isBeerOClock, hLeft, mLeft, sLeft, msLeft } = this.state;
+    let { isBeerOClock, hLeft, mLeft, sLeft, msLeft } = this.props;
     let resultStyle, message;
     let messageOn = "Beer o'clock will be over in:";
     let messageOff = "Beer o'clock starts in:";
